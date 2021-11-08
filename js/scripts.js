d559a4c3279393ebd5f85f84e40da0bae94ff4d0
julienleonardo <julien.m.leonardo@gmail.com>
2021-11-08 14:48:06 -0800

    add answer class to html, created branches on JQ

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,41 @@
-$(document).ready(function() {
-  $(".radio").click(function() {
-      const lang = $("input:radio[name=lang]:checked").val();
-      let result 
-      if (lang === "java") {
-        $(".java").toggle();
-      } else if (lang === "hc") {
-        $(".hc").toggle();
-      } else if (lang === "c") {
-        $(".c").toggle();
-      }
+function addQuestions(q1, q2, q3, q4, q5) {
+  return q1 + q2 + q3 + q4 + q5;
+}
 
-    });
-  });
-  
-  const lang = $("#lang").val();
+$(document).ready(function () {
+  $('form').submit(function (e) {
+    e.preventDefault();
+
+    const questionOne = parseInt($('#question1').val());
+    const questionTwo = parseInt($('#question2').val());
+    const questionThree = parseInt($('#question3').val());
+    const questionFour = parseInt($('#question4').val());
+    const questionFive = parseInt($('#question5').val());
+    const result = addQuestions(questionOne, questionTwo, questionThree, questionFour, questionFive);
+
+    if (result === 5 || result < 8) {
+      return $('.answer').text('You should try JavaScript!');
+    } else if (result === 8 || result < 12) {
+      return $('.answer').text('You should try HTML/CSS!');
+    } else {
+      return $('.answer').text('You should try C#')
+    }
+  })
+});
+  // $(".radio").click(function() {
+  //     const lang = $("input:radio[name=lang]:checked").val();
+  //     let result 
+  //     if (lang === "java") {
+  //       $(".java").toggle();
+  //     } else if (lang === "hc") {
+  //       $(".hc").toggle();
+  //     } else if (lang === "c") {
+  //       $(".c").toggle();
+  //     }
+
+
+
+  //   });
+
+
+
